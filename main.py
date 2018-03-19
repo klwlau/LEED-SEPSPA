062352a7336298434f2d741cc5fce33e4c148aa1
Wilson.L <klwilsonlau@gmail.com>
2018-03-19 17:24:58 +0800

    put fit curve part into fitCurve func

--- main.py
+++ main.py
@@ -10,32 +10,7 @@ mask = makeMask(125, 125, 0, 30)
 centerArray = findSpot(fileArray, 100, mask, scaleFactor=10,showSpots=True, plotSensitivity=4)
 
 
-
-
-mask = makeMask(125, 125, 0, 30)
-
 fitCurve(fileArray,centerArray)
-# for i in range(len(centerArray)):
-#     spotNumber = i
-#     # print(centerArray[spotNumber])
-#     cropRange = 8
-#     spot1 = fileArray[int(centerArray[spotNumber][1]) - cropRange:int(centerArray[spotNumber][1]) + cropRange,\
-#             int(centerArray[spotNumber][0]) - cropRange:int(centerArray[spotNumber][0]) + cropRange]
-#         # plotFunc(spot1)
-#
-#
-#     cropedArray = spot1
-#     xyzArray = []
-#     for i in range(len(cropedArray)):
-#         for j in range(len(cropedArray[i])):
-#             xyzArray.append([i, j, cropedArray[i][j]])
-#
-#     x, y, z = np.array(xyzArray).T
-#     xy = x, y
-#     i = z.argmax()
-#     guess = [z[i], x[i], y[i], 50, 50, 100, 30, 30, 100]
-#     pred_params, uncert_cov = curve_fit(fitFunc, xy, z, p0=guess, method='lm')
-#     # plotFitFunc(xy, 11, pred_params)
-#     print(pred_params)
+
 
 print("done")
