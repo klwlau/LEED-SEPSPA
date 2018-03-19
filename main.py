3223fe54bf7c507fcb4b3346a1fde4726a57d85d
Wilson.L <klwilsonlau@gmail.com>
2018-03-19 17:52:51 +0800

    add comment

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from UsedFunc import *
 rcParams['figure.figsize'] = [10., 8.]
 
 
-#read Image and make Mask
+#int parameter, make Mask
 fileArray = readLEEDImage("test2.tif")# to set the picWidth,picHeight for findSpot function
 mask = makeMask(125, 125, 0, 30)
-centerArray = findSpot(fileArray, 100, mask, scaleFactor=10,showSpots=True, plotSensitivity=4)
 
+
+#potential mainloop
+fileArray = readLEEDImage("test2.tif")
+centerArray = findSpot(fileArray, 100, mask, scaleFactor=10,showSpots=False, plotSensitivity=4)
 
 fitCurve(fileArray,centerArray)
 
