db81ac57031b4333adbdedbd06b57db405d38504
Wilson.L <klwilsonlau@gmail.com>
2018-03-20 17:03:12 +0800

    bug fix

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from UsedFunc import *
 import glob
 import datetime
 import time
+
 rcParams['figure.figsize'] = [10., 8.]
 
 
@@ -23,19 +24,16 @@ def mainLoop():
 
 
 # int parameter, make Mask
-timeStamp=datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M%S')
-CSVName=timeStamp+".csv"
-folderName="20180212_scan01"
-fileList = glob.glob("./"+folderName+"/*.tif")
-setPicDim(fileList[0]) # to set the picWidth,picHeight for findSpot function
-mask = makeMask(470, 440, 250, 300) # int mask
-writeBuffer =20
-
+timeStamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M%S')
+CSVName = timeStamp + ".csv"
+folderName = "20180212_scan01"
+fileList = glob.glob("./" + folderName + "/*.tif")
+setPicDim(fileList[0])  # to set the picWidth,picHeight for findSpot function
+mask = makeMask(470, 440, 250, 300)  # int mask
+writeBuffer = 20
 
 # findSpot(fileList[9], 20, mask, scaleFactor=1,showSpots=True,plotSensitivity=4)
 
 mainLoop()
-
-
 
 print("done")
