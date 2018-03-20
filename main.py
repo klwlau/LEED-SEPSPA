1454a5722aea74ffb2bfd9b61a293e61b0ceb0be
Wilson.L <klwilsonlau@gmail.com>
2018-03-20 14:10:48 +0800

    rewrite main loop

--- main.py
+++ main.py
@@ -6,6 +6,21 @@ import time
 rcParams['figure.figsize'] = [10., 8.]
 
 
+def mainLoop():
+    writeBufferArray = []
+    counter = 0;
+    fileAmount = len(fileList)
+    for fileName in fileList:
+        counter += 1
+        templist, elements = findSpot(fileName, 130, mask, scaleFactor=1, showSpots=False)
+        writeBufferArray.append(templist)
+        print(counter, ",", elements, ",", fileName, ",", counter / fileAmount * 100, "%")
+        if counter % writeBuffer == 0:
+            saveToCSV(writeBufferArray, CSVName)
+            writeBufferArray = []
+        if counter == fileAmount:
+            saveToCSV(writeBufferArray, CSVName)
+
 
 # int parameter, make Mask
 timeStamp=datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M%S')
@@ -13,21 +28,13 @@ CSVName=timeStamp+".csv"
 folderName="20180212_scan01"
 fileList = glob.glob("./"+folderName+"/*.tif")
 setPicDim(fileList[0]) # to set the picWidth,picHeight for findSpot function
-mask = makeMask(450, 450, 0, 100) # int mask
+mask = makeMask(450, 450, 250, 300) # int mask
 writeBuffer =20
 
-# need to rewrite mainloop
-writeBufferArray=[]
-counter=0;fileAmount= len(fileList)
-for fileName in fileList:
-    counter += 1
-    templist,elements = findSpot(fileName, 150, mask, scaleFactor=1,showSpots=False)
-    writeBufferArray.append(templist)
-    print(counter,",",elements,",",fileName,",",counter/fileAmount*100,"%")
-    if counter % writeBuffer == 0:
-        saveToCSV(writeBufferArray, CSVName)
-        writeBufferArray = []
-    if counter == fileAmount:
-        saveToCSV(writeBufferArray, CSVName)
 
+findSpot(fileList[0], 130, mask, scaleFactor=1,showSpots=True)
+
+
+# # need to rewrite mainloop
+# mainLoop()
 print("done")
