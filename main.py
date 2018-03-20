3720abe1430d02906fe9f83644197f6b8d20438b
Wilson.L <klwilsonlau@gmail.com>
2018-03-20 13:16:16 +0800

    added write buffer as a parameter rename rowarray as writebufferarray

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from matplotlib import rcParams
 from UsedFunc import *
 import glob
-
 rcParams['figure.figsize'] = [10., 8.]
 
 
@@ -11,21 +10,20 @@ CSVName="spot.csv"
 fileList = glob.glob("./*.tif")
 setPicDim("test2.tif") # to set the picWidth,picHeight for findSpot function
 mask = makeMask(125, 125, 0, 30)
+writeBuffer =20
 
 # need to rewrite mainloop
-RowArray=[]
+writeBufferArray=[]
 counter=0;fileAmount= len(fileList)
 for fileName in fileList:
     counter += 1
-    templist=createRowArray(fileName, mask)
-
-    RowArray.append(templist)
-    print(templist)
-    if counter%10 == 0:
-        saveToCSV(RowArray,CSVName)
-        RowArray = []
+    templist = createRowArray(fileName, mask)
+    writeBufferArray.append(templist)
+    print(counter/fileAmount*100,"%")
+    if counter % writeBuffer == 0:
+        saveToCSV(writeBufferArray, CSVName)
+        writeBufferArray = []
     if counter == fileAmount:
-        saveToCSV(RowArray,CSVName)
-# createSaveArray("test2.tif")
+        saveToCSV(writeBufferArray, CSVName)
 
 print("done")
