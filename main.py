0e9ea2fe9be8bf5ea7f613b22a68eb492611153a
Wilson.L <klwilsonlau@gmail.com>
2018-03-20 14:01:26 +0800

    testing

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ rcParams['figure.figsize'] = [10., 8.]
 # int parameter, make Mask
 timeStamp=datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M%S')
 CSVName=timeStamp+".csv"
-folderName="20180213_scan02"
+folderName="20180212_scan01"
 fileList = glob.glob("./"+folderName+"/*.tif")
 setPicDim(fileList[0]) # to set the picWidth,picHeight for findSpot function
-mask = makeMask(125, 125, 0, 30) # int mask
+mask = makeMask(450, 450, 0, 100) # int mask
 writeBuffer =20
 
 # need to rewrite mainloop
@@ -21,7 +21,7 @@ writeBufferArray=[]
 counter=0;fileAmount= len(fileList)
 for fileName in fileList:
     counter += 1
-    templist = findSpot(fileName, 18, mask, scaleFactor=10)
+    templist = findSpot(fileName, 100, mask, scaleFactor=1,showSpots=False)
     writeBufferArray.append(templist)
     print(counter,",",fileName,",",counter/fileAmount*100,"%")
     if counter % writeBuffer == 0:
