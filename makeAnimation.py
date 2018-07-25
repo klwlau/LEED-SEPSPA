80bc9d26ed02287df60687dcd4505be59febbbda
Wilson.L <klwilsonlau@gmail.com>
2018-07-25 11:28:23 +0800

    add makeAnimation

---
+++ makeAnimation.py
@@ -0,0 +1,36 @@
+import matplotlib.animation as animation
+from UsedFunc import *
+import glob
+
+fig = plt.figure()
+plotSensitivity=3
+ims= []
+startID = 0
+dataFolderName = configList["dataFolderName"]
+fileList = glob.glob(dataFolderName + "/*.tif")
+fileList = sorted(fileList)
+counter = 0
+
+for filePath in fileList:
+    plot_data = readLEEDImage(filePath)
+    m, s = np.mean(plot_data), np.std(plot_data)
+    im = plt.imshow(plot_data, interpolation='nearest', cmap='jet',
+                    vmin=m - plotSensitivity * s, vmax=m + plotSensitivity * s,
+                    origin='lower')
+    ims.append([im])
+    print(counter)
+    counter+=1
+
+
+
+
+
+
+
+print("making animation")
+ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
+                                repeat_delay=1000)
+print("saving animation")
+ani.save('dynamic_images.mp4')
+print("ploting animation")
+plt.show()
