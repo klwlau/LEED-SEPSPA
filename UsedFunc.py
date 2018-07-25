8c5fb4953c3ad957bb84e279e8829788487b7f28
Wilson.L <klwilsonlau@gmail.com>
2018-07-25 16:10:06 +0800

    fix plotFitFunc bug

--- UsedFunc.py
+++ UsedFunc.py
@@ -193,9 +193,16 @@ def getSpotRoughRange(imgArray: np.array, searchThreshold: float, mask: np.array
         return returnArray
 
 
+# def plotFitFunc(fit_params):
+#     xi, yi = np.mgrid[:cropRange * 2:30j, :cropRange * 2:30j]
+#     xyi = np.vstack([xi.ravel(), yi.ravel()])
+#
+#     zpred = fitFunc(xyi, *fit_params)
+#     zpred.shape = xi.shape
+
 def plotFitFunc(fit_params):
 
-    xi, yi = np.mgrid[:cropRange * 2:30j, :cropRange * 2:30j]
+    xi, yi = np.mgrid[fit_params[1]-cropRange:fit_params[1]+cropRange:30j,fit_params[2]-cropRange:fit_params[2]+cropRange:30j]
     xyi = np.vstack([xi.ravel(), yi.ravel()])
 
     zpred = fitFunc(xyi, *fit_params)
