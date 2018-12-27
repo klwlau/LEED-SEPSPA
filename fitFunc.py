e6ac33b9bdb6597261a141397a977b7789a9e706
Wilson.L <klwilsonlau@gmail.com>
2018-12-27 15:18:22 +0800

    test fit N gauss

--- fitFunc.py
+++ fitFunc.py
@@ -30,12 +30,25 @@ def fitFunc(xy, Amp, x_0, y_0, sigma_x, sigma_y, theta, A, B, C):
     g += backGroundEstimatedPlane(x, y, A, B, C)
     return g
 
+
 @jit
-def fit2Gauss(xy, Amp_1, x_0_1, y_0_1, sigma_x_1, sigma_y_1, theta_1, Amp_2, x_0_2, y_0_2, sigma_x_2, sigma_y_2,
-              theta_2, A, B, C):
+def fit2GaussFunc(xy, Amp_1, x_0_1, y_0_1, sigma_x_1, sigma_y_1, theta_1, Amp_2, x_0_2, y_0_2, sigma_x_2, sigma_y_2,
+                  theta_2, A, B, C):
     x, y = xy
     g = gauss2D(x, y, Amp_1, x_0_1, y_0_1, sigma_x_1, sigma_y_1, theta_1)
     g += gauss2D(x, y, Amp_2, x_0_2, y_0_2, sigma_x_2, sigma_y_2, theta_2)
+    g += backGroundEstimatedPlane(x, y, A, B, C)
+    return g
+
+
+@jit
+def listFitNGaussFunc(xy, A, B, C, *gaussParameterList):
+    x, y = xy
+    g = 0
+    print(gaussParameterList)
+    for parameters in gaussParameterList:
+        g += gauss2D(x, y, parameters[0], parameters[1], parameters[2], parameters[3], parameters[4], parameters[5])
+
     g += backGroundEstimatedPlane(x, y, A, B, C)
     return g
 
