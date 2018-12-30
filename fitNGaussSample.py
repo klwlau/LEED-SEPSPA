3e29a6fa1bf1b185ce1b22fd90308d04b17e2be7
Wilson.L <klwilsonlau@gmail.com>
2018-12-30 16:54:23 +0800

    make guessBound with N gaussian

--- fitNGaussSample.py
+++ fitNGaussSample.py
@@ -11,11 +11,11 @@ ySpace = np.linspace(-10, 10, 100)
 xi, yi = np.meshgrid(xSpace, ySpace)
 xyi = np.vstack([xi.ravel(), yi.ravel()])
 
-guessUpBound = [15, 15, 5000, 100000, 10, 10, 30, 30, 180, 100000, 10, 10, 30, 30, 180]
-guessLowBound = [-15, -15, 0, 0, -10, -10, 0.001, 0.001, -180, 0, -10, -10, 0.001, 0.001, -180]
+backgroundGuessUpBound = [15, 15, 5000]
+backgroundGuessLowBound = [-15, -15, 0]
 
-guessBound = [guessLowBound, guessUpBound]
-
+guessUpBoundTemp = [100000, 10, 10, 30, 30, 180]
+guessLowBoundTemp = [0, -10, -10, 0.001, 0.001, -180]
 
 
 def plottingOriFunc():
@@ -25,17 +25,35 @@ def plottingOriFunc():
     plt.show()
 
 
+def genBound(numOfGauss):
+    # , backgroundGuessUpBound=backgroundGuessUpBound, backgroundGuessLowBound=backgroundGuessLowBound,
+    #              guessUpBoundTemp=guessUpBoundTemp, guessLowBoundTemp=guessLowBoundTemp):
+
+
+    guessUpBound = backgroundGuessUpBound
+    guessLowBound = backgroundGuessLowBound
+
+    for num in range(numOfGauss):
+        guessUpBound += guessUpBoundTemp
+        guessLowBound += guessLowBoundTemp
+
+    return [guessLowBound, guessUpBound]
+
+
 def fittingTest():
     # for i in range(1):
     numberOfGauss = 2
     fittingParamsSampleArray = np.ones(numberOfGauss * 6 + 3)
-    fit_params, uncert_cov = curve_fit(NGauss(numberOfGauss), xyi, zSpace,
-                                       tuple(fittingParamsSampleArray), bounds=guessBound)
+    testguessBound = genBound(numberOfGauss)
+    print(len(fittingParamsSampleArray), len(testguessBound[0]), len(testguessBound[1]))
 
-for i in range(100):
+    # fit_params, uncert_cov = curve_fit(NGauss(numberOfGauss), xyi, zSpace,
+    #                                    tuple(fittingParamsSampleArray), bounds=genBound(numberOfGauss))
+
+
+for i in range(3):
     zSpace = NGauss(2)(xyi, 0, 2, 0, 300, 0, 5, 4, 2, 120, 300, 3, 0, 1, 2, 120)
     zSpace += + np.random.rand(len(zSpace))
     timer.tic()
     fittingTest()
     timer.toc()
-
