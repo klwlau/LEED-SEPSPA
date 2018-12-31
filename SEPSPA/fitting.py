d560f83b2f4eefa7aa17a12cd53d223f38095864
Wilson <klwilsonlau@gmail.com>
2018-12-31 16:42:52 +0800

    add applyMask,plotSpots,getSpotRoughRange

--- SEPSPA/fitting.py
+++ SEPSPA/fitting.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
 from pytictoc import TicToc
 import csv, itertools, json, os, shutil, ntpath
+from numba import jit
 import numpy as np
 
 class fitting:
@@ -25,6 +26,7 @@ class fitting:
         else:
             self.fileList = glob.glob(self.dataFolderName + "/*.tif")
             self.fileList = sorted(self.fileList)
+        self.CSVwriteBuffer = self.configList["CSVwriteBuffer"]
 
     def preStart(self):
         self.makeResultDir()
@@ -88,9 +90,74 @@ class fitting:
                 if (x - mask_x_center) ** 2 + (y - mask_y_center) ** 2 > r1 ** 2 and (x - mask_x_center) ** 2 + (
                         y - mask_y_center) ** 2 < r2 ** 2:
                     mask[y][x] = 1
-
         return np.array(mask).astype(np.uint8)
 
+    def applyMask(self,imageArray):
+        """apply the mask to an np array"""
+        appliedMask = np.multiply(imageArray, self.mask)
+        return appliedMask
+
+    def plotSpots(self,imgArray, objects_list, plotSensitivity_low=0.0, plotSensitivity_up=0.5,
+                  saveMode=False, saveFileName="test", showSpots=False):
+        """plot sep result"""
+        fig, ax = plt.subplots()
+        min_int, max_int = np.amin(imgArray), np.amax(imgArray)
+        plt.imshow(imgArray, interpolation='nearest', cmap='jet',
+                   vmin=min_int + (max_int - min_int) * plotSensitivity_low,
+                   vmax=min_int + (max_int - min_int) * plotSensitivity_up,
+                   origin='lower')
+
+        # plot an ellipse for each object
+        for i in range(len(objects_list)):
+            e = Ellipse(xy=(objects_list['x'][i], objects_list['y'][i]),
+                        width=3 * objects_list['a'][i],
+                        height=3 * objects_list['b'][i],
+                        angle=objects_list['theta'][i] * 180. / np.pi)
+            e.set_facecolor('none')
+            e.set_edgecolor('red')
+            ax.add_artist(e)
+
+        plt.colorbar()
+        if saveMode:
+            savePath = configList["saveFigModeParameters"]["saveFigFolderName"]
+            plt.savefig(savePath + saveFileName + ".png")
+
+        if showSpots:
+            plt.show()
+        else:
+            plt.clf()
+
+    @jit
+    def getSpotRoughRange(self,imgArray: np.array, searchThreshold: float, mask: np.array, scaleDownFactor: float = 10,
+                          plotSensitivity_low: float = 0.0, plotSensitivity_up: float = 0.5,
+                          showSpots: bool = False, fittingMode: bool = False, saveMode=False, printReturnArray=False,
+                          saveFileName="test") -> np.array:
+
+        imgArray = self.applyMask(imgArray, mask)
+
+        bkg = sep.Background(imgArray)
+        sep_objects_list = sep.extract(imgArray, searchThreshold, err=bkg.globalrms)
+
+        if showSpots is True or saveMode is True:
+            self.plotSpots(imgArray, sep_objects_list, plotSensitivity_low, plotSensitivity_up,
+                      showSpots=showSpots, saveMode=saveMode, saveFileName=saveFileName)
+
+        if fittingMode is True:
+            returnArray = np.array([sep_objects_list['xcpeak'], sep_objects_list['ycpeak']]).T
+
+            if printReturnArray:
+                print(len(returnArray))
+                print(returnArray)
+            return returnArray, sep_objects_list
+
+        else:
+            returnArray = np.array([sep_objects_list['peak'], sep_objects_list['x'], sep_objects_list['y'],
+                                    sep_objects_list['xmax'], sep_objects_list['ymax'],
+                                    sep_objects_list['a'], sep_objects_list['b'], sep_objects_list['theta']]).T
+            if printReturnArray:
+                print(len(returnArray))
+                print(returnArray)
+            return returnArray,sep_objects_list
 
 
 
@@ -102,7 +169,20 @@ class fitting:
         print("TestMode")
 
     def SEPMode(self):
+        self.SEPDict ={}
         print("SEPMode")
+        writeBufferArray =[]
+        FileHeader=["FileID", "File Name", "Number of Spots"]
+        SEPparameterArrray=["Am", "x", "y", "xpeak", "ypeak", "a", "b", "theta"]
+
+        for filePath in self.fileList:
+
+
+
+
+        print("save to :" + self.CSVName)
+        return self.SEPDict
 
     def SPAMode(self):
         print("SPAMode")
+        print("save to :" + self.CSVName)
