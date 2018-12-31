6115aeec0fe82aee82f11e7b7228f6fe0b914902
Wilson <klwilsonlau@gmail.com>
2018-12-31 10:24:09 +0800

    start coding __init__

--- SEPSPA/fitting.py
+++ SEPSPA/fitting.py
@@ -1,15 +1,28 @@
 class fitting:
-    from scipy.optimize import curve_fit
-    import sep
-    from PIL import Image
-    import matplotlib.pyplot as plt
-    from matplotlib.patches import Ellipse
-    from pytictoc import TicToc
-    import csv, itertools, json, os, shutil, ntpath
 
-    def __int__(self,configFilePath="configList.json"):
+
+    def __init__(self,configFilePath="configList.json"):
+        import time
+        import glob
+        start_time = time.time()
+        print("Program Started, Loading Libraries")
+        import datetime
+        print("Start Loading UsedFunc")
+        from scipy.optimize import curve_fit
+        import sep
+        from PIL import Image
+        import matplotlib.pyplot as plt
+        from matplotlib.patches import Ellipse
+        from pytictoc import TicToc
+        import csv, itertools, json, os, shutil, ntpath
+
         self.configFilePath = configFilePath
+        self.configList = json.load(open(self.configFilePath))
 
+
+    def printConfigList(self):
+        for i in self.configList:
+            print (i)
 
     def testMode(self):
         print("TestMode")
